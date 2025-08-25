5f42b9b6b11530a92cc7a1c56951bf8a87f37f98
Henilp1603 <henil1603@gmail.com>
2025-08-25 06:31:27 -0500

    h09_agents_setup

--- src/modules/Home/ui/views/HomeView.tsx
+++ src/modules/Home/ui/views/HomeView.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import { authClient } from "@/lib/auth-client";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+// import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
 
 import React from "react";
 
 const HomeView = () => {
   const { data: session } = authClient.useSession();
 
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Henil" }));
+  // const trpc = useTRPC();
+  // const { data } = useQuery(trpc.hello.queryOptions({ text: "Henil" }));
 
   if (!session) {
     return <p>Loading...</p>;
   }
-  return <div className="flex flex-col gap-y-4 p-4">{data?.greeting}</div>;
+  return <div className="flex flex-col gap-y-4 p-4">Home</div>;
 };
 
 export default HomeView;
