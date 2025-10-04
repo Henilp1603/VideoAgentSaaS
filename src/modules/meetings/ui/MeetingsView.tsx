840b9940612436ab71cf005e3f17f79e410ba1ca
Henilp1603 <henil1603@gmail.com>
2025-10-04 14:12:55 -0500

    h18_meeting_data_table

--- src/modules/meetings/ui/MeetingsView.tsx
+++ src/modules/meetings/ui/MeetingsView.tsx
@@ -4,12 +4,25 @@ import ErrorState from "@/components/ErrorState";
 import LoadingState from "@/components/LoadingState";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
+import EmptyState from "@/components/EmptyState";
 
 const MeetingsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
-  return <div>{JSON.stringify(data?.items)}</div>;
+  return (
+    <div className="flex flex-1 pb-4 md:px-8 flex-col gap-y-4">
+      <DataTable data={data.items} columns={columns} />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create Your First Meeting"
+          description="Schedule a meeting to connect with other. Each meeting leta you collaborate, share ideas, and interact with participants in real time."
+        />
+      )}
+    </div>
+  );
 };
 
 export default MeetingsView;
